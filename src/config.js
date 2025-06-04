0de6e02559c79de40c1cd874710e8f6568e42bff
lifegatejustice <lifegatedetom@gmail.com>
2025-06-04 15:28:05 +0100

    `Update config.js to use environment variables for API keys`

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const TMDB_API_KEY = "d4959a464b2f03274d291417971935f8";
+export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const WATCHMODE_API_KEY = ""; // Add your Watchmode API key here
+export const WATCHMODE_API_KEY = import.meta.env.VITE_WATCHMODE_API_KEY;
